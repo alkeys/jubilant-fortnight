06077dc88691db3fa53285080526bec37bd99e77
alkeys <am18007@ues.edu.sv>
2024-03-26 04:16:56 +0600

    Proyectos de alura

---
+++ Monedas/js/conv.js
@@ -0,0 +1,47 @@
+/*
+
+
+
+    ,---,.                                 ,--,         ,-.
+  ,'  .'  \                              ,--.'|     ,--/ /|
+,---.' .' |                              |  | :   ,--. :/ |
+|   |  |: |                              :  : '   :  : ' /                       .--.--.
+:   :  :  /     .--,            ,--.--.  |  ' |   |  '  /      ,---.       .--, /  /    '
+:   |    ;    /_ ./|           /       \ '  | |   '  |  :     /     \    /_ ./||  :  /`./
+|   :     \, ' , ' :          .--.  .-. ||  | :   |  |   \   /    /  |, ' , ' :|  :  ;_
+|   |   . /___/ \: |           \__\/: . .'  : |__ '  : |. \ .    ' / /___/ \: | \  \    `.
+'   :  '; |.  \  ' |           ," .--.; ||  | '.'||  | ' \ \'   ;   /|.  \  ' |  `----.   \
+|   |  | ;  \  ;   :          /  /  ,.  |;  :    ;'  : |--' '   |  / | \  ;   : /  /`--'  /
+|   :   /    \  \  ;         ;  :   .'   \  ,   / ;  |,'    |   :    |  \  \  ;'--'.     /
+|   | ,'      :  \  \        |  ,     .-./---`-'  '--'       \   \  /    :  \  \ `--'---'
+`----'         \  ' ;         `--`---'                        `----'      \  ' ;
+                `--`                                                       `--`
+*/
+function  ConvertidorMonedas(){
+    var cantidad = parseFloat(document.getElementById("cantidad").value);
+    var monedaOrigen = document.getElementById("de").value;
+    var monedaDestino = document.getElementById("a").value;
+
+    const monedas = {
+        "dolar": 1,
+        "euro": 0.92,
+        "yen": 151,
+        "pesoMx":16,
+        "yuan": 7.21
+    };
+    document.getElementById("CerrarVentana").addEventListener("click",cerrarVentana);
+    var elementoVentas= document.getElementById("window-notice");
+    document.getElementById("ResultadoCovercion").textContent="El resultado es: " + cantidad * monedas[monedaDestino] / monedas[monedaOrigen];
+    elementoVentas.style.display = "flex";
+
+}
+function cerrarVentana(){
+    var elementoVentas= document.getElementById("window-notice");
+    elementoVentas.style.display = "none";
+}
+
+function limpiarDatosFormulario(){
+    document.getElementById("cantidad").value="";
+    document.getElementById("de").value="";
+    document.getElementById("a").value="";
+}
